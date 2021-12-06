c920b23701c23beda310c0482bd6d1a45fbe2219
waiz01 <2020628062@student.uitm.edu.my>
2021-12-06 22:11:50 +0800

    LAB5

---
+++ app/src/main/java/com/example/lab5authent/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.lab5authent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openRegister(View v){
+        Intent i = new Intent(this,RegisterActivity.class);
+        startActivity(i);
+    }
+
+    public void openLogin(View v){
+        Intent i = new Intent(this,LoginActivity.class);
+        startActivity(i);
+}}
